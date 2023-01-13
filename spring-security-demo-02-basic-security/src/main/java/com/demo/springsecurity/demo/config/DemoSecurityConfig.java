7b2dc5b4f9aa6b550523b6fa5c4e25a3dd05d890
jitendrapitchuka <jithendra2900@gmail.com>
2023-01-14 00:13:31 +0530

    spring scurity upto logout

---
+++ spring-security-demo-02-basic-security/src/main/java/com/demo/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.demo.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig  {
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+    	
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}john")
+            .roles("EMPLOYEE")
+            .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}mary")
+                .roles("MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}susan")
+                .roles("ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(john, mary, susan);	
+
+	}
+	
+}
