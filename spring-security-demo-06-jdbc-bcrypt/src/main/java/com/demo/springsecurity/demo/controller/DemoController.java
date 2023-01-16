3703ca5594af8169a3079ae7570028e36d5f7590
jitendrapitchuka <jithendra2900@gmail.com>
2023-01-16 23:37:44 +0530

    spring rest basic helloworld

---
+++ spring-security-demo-06-jdbc-bcrypt/src/main/java/com/demo/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.demo.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+	
+	
+	@GetMapping("/leaders")
+	public String leaders() {
+		
+		return "leaders";
+	}
+	
+	@GetMapping("/systems")
+	public String showAdmins() {
+		
+		return "systems";
+	}
+}
